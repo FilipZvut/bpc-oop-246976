2cb6f0a5539c047fca20374df1d41fac72ad632b
FilipZvut <246976@vutbr.cz>
2024-02-15 17:45:14 +0100

    complex class done

---
+++ cv02/Complex.cs
@@ -0,0 +1,56 @@
+﻿
+class Complex
+{
+    public double Realna;
+    public double Imaginarni;
+    public Complex(double realna=0.0, double imaginarni = 0.0)
+    {
+        Realna = realna;
+        Imaginarni = imaginarni;
+    }
+    
+    public static Complex operator +(Complex a, Complex b)
+    {
+        return new Complex(a.Realna + b.Realna, a.Imaginarni + b.Imaginarni);
+    }
+
+    public static Complex operator -(Complex a, Complex b)
+    {
+        return new Complex(a.Realna - b.Realna, a.Imaginarni - b.Imaginarni);
+    }
+
+    public static Complex operator *(Complex a, Complex b)
+    {
+        return new Complex(a.Realna * b.Realna, a.Imaginarni * b.Imaginarni);
+    }
+
+    public static Complex operator /(Complex a, Complex b)
+    {
+        return new Complex(a.Realna / b.Realna, a.Imaginarni / b.Imaginarni);
+    }
+
+    public static bool operator ==(Complex a, Complex b)
+    {
+        return a.Realna == b.Realna && a.Imaginarni == b.Imaginarni;
+    }
+
+    public static bool operator !=(Complex a, Complex b)
+    {
+        return !(a.Realna == b.Realna && a.Imaginarni == b.Imaginarni);
+    }
+
+    public static Complex operator -(Complex a)
+    {
+        return new Complex(-a.Realna, -a.Imaginarni);
+    }
+    public override string ToString()
+    {
+        
+        if(Imaginarni >= 0)
+            return $"{Realna} + j{Imaginarni}";
+        else
+            return $"{Realna} - j{Imaginarni}";       
+    }
+
+
+}
--- cv02/Program.cs
+++ cv02/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+using System.Diagnostics;
+using System.Security.Cryptography.X509Certificates;
+
+Complex cislo1 = new Complex(1.0, 1.0);
+Complex cislo2 = new Complex(1.0, 1.0);
+
+
+int c = 10;
+string cc;
+
+cc = c.ToString();
+
+
+Debug.WriteLine(c);

4f441c82bcc0db8ae14a0578bc6a9c4aab74a4f1
FilipZvut <246976@vutbr.cz>
2024-02-26 12:52:35 +0100

    cc

---
+++ cv04/Program.cs
@@ -0,0 +1,19 @@
+﻿class program
+{
+    static void Main(string[] args)
+    {
+        string testovaciText = "Toto je retezec predstavovany nekolika radky,\n"
+                             + "ktere jsou od sebe oddeleny znakem LF (Line Feed).\n"
+                             + "Je tu i nejaky ten vykricnik! Pro ucely testovani i otaznik?\n"
+                             + "Toto je jen zkratka zkr. ale ne konec vety. A toto je\n"
+                             + "posledni veta!";
+
+        StringStatistics str = new StringStatistics(testovaciText);
+        Console.WriteLine(testovaciText+"\n");
+
+        Console.WriteLine(str.PocetSlov());
+        Console.WriteLine(str.PocetRadku());
+        Console.WriteLine(str.PocetVet());
+        Console.WriteLine(str.NejdelsiSlovo());
+    }
+}
---
+++ cv04/StringStatistics.cs
@@ -0,0 +1,82 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+class StringStatistics
+{
+    string str;
+    public StringStatistics(string strstats)
+    {
+        str = strstats;
+    }
+
+    public int PocetSlov()
+    {
+        int pocet = 1;
+        foreach (char c in str)
+        {
+            if (c == ' '|| c=='\n')
+            {
+                pocet++;
+            }
+        }
+        return pocet;
+    }
+    public int PocetRadku()
+    {
+        int pocet = 1;
+        foreach (char c in str)
+        {
+            if (c == '\n')
+            {
+                pocet++;
+            }
+        }
+        return pocet;
+    }
+    public int PocetVet()
+    {
+        bool znamenko=false;
+        int pocet = 1;
+        foreach (char c in str)
+        {
+            if(c == '.' || c == '!' || c == '?')
+            {
+                znamenko=true;
+            }
+
+
+            if(znamenko&& char.IsUpper(c))
+            {
+                pocet++;
+                znamenko = false;
+            }
+            else if (znamenko && char.IsLower(c))
+            {
+                znamenko = false;
+            }
+        }
+
+        return pocet;
+    }
+    public string NejdelsiSlovo()
+    {
+        string nejdelsi = "";
+        string slovo = "";
+        int pismena = 0;
+        
+        foreach (char c in str)
+        {
+            slovo += c;
+            pismena++;
+            if (c == ' ' || c=='!' || c=='.' || c=='?' )
+            {
+                if(pismena < slovo.Length)
+                    nejdelsi = slovo;
+
+                slovo = "";
+                pismena = 0;
+            }
+        }
+        return nejdelsi;
+    }
+}
+

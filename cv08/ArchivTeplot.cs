3920ed00bc111dd76a109d2429685e4659e25817
FilipZvut <246976@vutbr.cz>
2024-03-28 15:23:24 +0100

    domaci priprava

---
+++ cv08/ArchivTeplot.cs
@@ -0,0 +1,40 @@
+﻿public class ArchivTeplot
+{   
+    public SortedDictionary<int, RocniTeplota> _archiv;
+
+    public ArchivTeplot()
+    {
+        _archiv = new();
+    }
+
+    public void Load(string path)
+    {
+        _archiv.Clear();
+       
+        foreach(string radek in File.ReadLines(path))
+        {
+            string[] data = radek.Split(':', ';');
+            int rok = int.Parse(data[0]);
+            List<double> teploty = data.Skip(1).Select(double.Parse).ToList();
+            _archiv.Add(rok, new RocniTeplota(rok, teploty));
+        }
+
+    }
+
+    public void Save(string path)
+    {
+        using (StreamWriter sw = new StreamWriter(path))
+        {
+            foreach(var hodnoty in _archiv.Values) 
+            {
+                sw.Write(hodnoty.Rok + ": ");
+                sw.WriteLine(string.Join("; ",hodnoty.MesicniTeploty.Select(T => T.ToString("0.0"))));
+            }
+        }
+
+        Console.WriteLine("Ulozeno");
+    }
+
+    public void Kalibrace
+
+}
---
+++ cv08/RocniTeplota.cs
@@ -0,0 +1,24 @@
+﻿public class RocniTeplota
+{
+    public int Rok;
+    public List<double> MesicniTeploty;
+
+    public RocniTeplota(int rok, List<double> mesicniTeploty)
+    {
+        Rok = rok;
+        MesicniTeploty = mesicniTeploty;
+    }
+    public double MaxTeplota
+    {
+        get { return MesicniTeploty.Max(); }
+    }
+    public double MinTeplota
+    {
+        get { return MesicniTeploty.Min(); }
+    }
+    public double PrumRocniTeplota
+    {
+        get { return MesicniTeploty.Average(); }
+    }
+}
+

fa09a8b63b6dc257397a34fa0fded9e3609a0f40
FilipZvut <246976@vutbr.cz>
2024-04-03 14:15:40 +0200

    cv08

--- cv08/ArchivTeplot.cs
+++ cv08/ArchivTeplot.cs
@@ -4,13 +4,13 @@
 
     public ArchivTeplot()
     {
-        _archiv = new();
+        _archiv = new SortedDictionary<int, RocniTeplota>();
     }
 
     public void Load(string path)
     {
         _archiv.Clear();
-       
+        
         foreach(string radek in File.ReadLines(path))
         {
             string[] data = radek.Split(':', ';');
@@ -31,10 +31,63 @@
                 sw.WriteLine(string.Join("; ",hodnoty.MesicniTeploty.Select(T => T.ToString("0.0"))));
             }
         }
-
         Console.WriteLine("Ulozeno");
     }
 
-    public void Kalibrace
+    public void Kalibrace(double konstanta)
+    {
+        foreach (var rocniTeplota in _archiv)
+        {
+            for (int i = 0; i < rocniTeplota.Value.MesicniTeploty.Count; i++)
+            {
+                rocniTeplota.Value.MesicniTeploty[i] += konstanta;
+            }
+        }
+    }
 
+    public RocniTeplota Vyhledej(int rok)
+    {
+        if (_archiv.ContainsKey(rok))
+            return _archiv[rok];
+        else
+            return null;
+    }
+
+    public void TiskTeplot()
+    {
+        foreach (var rocniteploty in _archiv)
+        {
+            Console.Write(rocniteploty.Key + ":   ");
+            Console.Write(string.Join("   ", rocniteploty.Value.MesicniTeploty.Select(T => T.ToString("0.0"))));
+            Console.WriteLine();
+        }
+        Console.WriteLine() ;
+    }
+
+    public void TiskPrumernychRocnichTeplot()
+    {
+        foreach (var rocniteploty in _archiv)
+        {
+            Console.Write(rocniteploty.Key + ":   ");
+            Console.Write(rocniteploty.Value.PrumRocniTeplota.ToString("0.0"));
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+
+    public void TiskPrumernychMesicnichTeplot()
+    {
+        for (int mesic = 0; mesic < 12; mesic++)
+        {
+            double prum = 0;
+            foreach (var rocniTeplota in _archiv.Values)
+            {
+                prum += rocniTeplota.MesicniTeploty[mesic];
+            }
+            prum = prum / _archiv.Count;
+            Console.WriteLine($"Mesic {mesic + 1}:\t {prum.ToString("0.0")} °C");
+        }
+        Console.WriteLine();
+    }
 }
+
--- cv08/Program.cs
+++ cv08/Program.cs
@@ -1,4 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-ArchivTeplot archivTeplot = new ArchivTeplot();
-archivTeplot.Load("Teploty.txt");
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        ArchivTeplot archiv = new ArchivTeplot();
+        archiv.Load("teploty.txt");
+    
+        Console.WriteLine("Původní teploty:");
+        archiv.TiskTeplot();
+    
+        Console.WriteLine("Tisk prumernych rocnich teplot:");
+        archiv.TiskPrumernychRocnichTeplot();
+
+        Console.WriteLine("Tisk prumernych mesicnich teplot:");
+        archiv.TiskPrumernychMesicnichTeplot();
+
+
+
+        archiv.Kalibrace(-0.1);
+        Console.WriteLine("Teploty po kalibraci:");
+        archiv.TiskTeplot();
+    
+        archiv.Save("kalibrovane_teploty.txt");
+        Console.WriteLine("Data byla uložena do souboru 'kalibrovane_teploty.txt'.");
+    }
+
+}

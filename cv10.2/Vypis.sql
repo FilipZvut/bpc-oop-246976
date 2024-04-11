470a9dcbc6ed9d1ff5fd3207f0036721c39d888a
FilipZvut <246976@vutbr.cz>
2024-04-11 18:19:22 +0200

    cv10 done

--- cv10.2/Vypis.sql
+++ cv10.2/Vypis.sql
@@ -1,7 +1,7 @@
 ﻿USE [D:\BPC-OOP\BPC-OOP-246976\CV10.2\VYUKA.MDF];
 GO
 
-SELECT Student.Jmeno, Student.Prijmeni, Predmet.Nazev
+SELECT Student.Jmeno, Student.Prijmeni, Predmet.Nazev AS Nazev_predmetu
 FROM Student
 LEFT JOIN Zapsani ON Student.Id = Zapsani.ID_studenta
 LEFT JOIN Predmet ON Zapsani.Zkratka_predmetu = Predmet.Zkratka;
@@ -11,14 +11,14 @@ FROM Student
 GROUP BY Prijmeni
 ORDER BY Pocet_studentu DESC;
 
-SELECT Predmet.Nazev, COUNT(*) AS Pocet_studentu
+SELECT Predmet.Nazev AS Nazev_predmetu, COUNT(*) AS Pocet_studentu
 FROM Predmet
 JOIN Zapsani ON Predmet.Zkratka = Zapsani.Zkratka_predmetu
 GROUP BY Predmet.Nazev
 HAVING COUNT(*) < 3;
 
 SELECT 
-    Predmet.Nazev,
+    Predmet.Nazev AS Nazev_predmetu,
     MIN(Hodnoceni.Hodnoceni) AS Nejlepsi_hodnoceni,
     MAX(Hodnoceni.Hodnoceni) AS Nejhorsi_hodnoceni,
     AVG(Hodnoceni.Hodnoceni) AS Prumerna_hodnoceni,
---
+++ cv10.2/data_pro_zapsani/dbo.StudentPredmet.data.sql
@@ -0,0 +1,6 @@
+INSERT INTO [dbo].[StudentPredmet] ([IdStudent], [ZkratkaPredmet]) VALUES (1000, N'BOOP ')
+INSERT INTO [dbo].[StudentPredmet] ([IdStudent], [ZkratkaPredmet]) VALUES (1000, N'BPC1T')
+INSERT INTO [dbo].[StudentPredmet] ([IdStudent], [ZkratkaPredmet]) VALUES (1001, N'BOOP ')
+INSERT INTO [dbo].[StudentPredmet] ([IdStudent], [ZkratkaPredmet]) VALUES (1002, N'BOOP ')
+INSERT INTO [dbo].[StudentPredmet] ([IdStudent], [ZkratkaPredmet]) VALUES (1003, N'BANA ')
+INSERT INTO [dbo].[StudentPredmet] ([IdStudent], [ZkratkaPredmet]) VALUES (1003, N'BPC1T')
